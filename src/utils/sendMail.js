837553a2c44f190cd934afdd75dfe1bd26086c5f
Imojenga <aina.jenko@gmail.com>
2025-06-21 15:35:50 +0300

    fix env

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,15 +1,24 @@
 import nodemailer from 'nodemailer';
 import { getEnvVar } from './getEnvVar.js';
 
-const transporter = nodemailer.createTransport({
-  host: getEnvVar('SMTP_HOST'),
-  port: getEnvVar('SMTP_PORT'),
-  auth: {
-    user: getEnvVar('SMTP_USER'),
-    pass: getEnvVar('SMTP_PASSWORD'),
-  },
-});
+let transporter;
+
+const configureTransporter = () => {
+  if (transporter) return transporter;
+
+  transporter = nodemailer.createTransport({
+    host: getEnvVar('SMTP_HOST'),
+    port: Number(getEnvVar('SMTP_PORT')),
+    auth: {
+      user: getEnvVar('SMTP_USER'),
+      pass: getEnvVar('SMTP_PASSWORD'),
+    },
+  });
+
+  return transporter;
+};
 
 export const sendMail = async (options) => {
-  return await transporter.sendMail(options);
+  const mailer = configureTransporter();
+  return await mailer.sendMail(options);
 };
